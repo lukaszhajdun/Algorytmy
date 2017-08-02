8323818919113bdb0a041b8bf7da9b459975563e
Lukasz <lukasz.hajdun@gmail.com>
2017-08-02 09:05:00 +0200

    Dodano algorytm "spacje camel case" (wszystko dziala)

--- src/Algorytmy/Algorithms/SpacjeCamelCase.java
+++ src/Algorytmy/Algorithms/SpacjeCamelCase.java
@@ -10,24 +10,23 @@ public class SpacjeCamelCase extends AbstractAlgorithm {
 
     @Override
     public void runAlgorithm(String[] input) {
-        int letters = 0;
+
         for (int wordsCounter = 1; wordsCounter < input.length; wordsCounter++) {
             String word = input[wordsCounter];
             char[] lettersInWord = word.toCharArray();
 
+            char pierwsza = lettersInWord[0];
+            System.out.print(Character.toString(pierwsza).toUpperCase());
 
-            System.out.println(lettersInWord);
+            for (int lettersCounter = 1; lettersCounter < lettersInWord.length; lettersCounter++) {
+
+                    System.out.printf(Character.toString(word.charAt(lettersCounter)).toLowerCase());
+                }
 
 
-//            for (int lettersCounter = 0; lettersCounter < lettersInWord.length; lettersCounter++) {
-//                if ( letters == 0) {
-//                    System.out.printf(Character.toString(word.charAt(lettersCounter)).toUpperCase());
-//                } else {
-//                    System.out.printf(Character.toString(word.charAt(lettersCounter)).toLowerCase());
-//                }
-//                letters++;
-//            }
 
+
+            }
         }
+
     }
-}
